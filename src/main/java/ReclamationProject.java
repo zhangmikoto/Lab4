e80e4add922064eb5f1322d6603b93c6c0faa82a
Geoffrey Challen <challen@illinois.edu>
2017-09-26 08:53:13 -0500

    More changes.

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -15,11 +15,12 @@ public class ReclamationProject
             String c = a; // TODO: set c to a
             a=b; b=c;}
         String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
+        /*
+         * For loop with i
+         */
         for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
                 for (int k = 0; k < b.length()- j; k++) {
                     r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
                         }} // Ah yeah
-        }
-        return r;
-    }
+        } return r; }
 }
